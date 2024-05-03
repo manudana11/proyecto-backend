35ea9734cfdb016a4e97a5a34eb7e098f160b5c3
manudana11 <manueldanapallardo@gmail.com>
2024-05-03 23:43:39 +0200

    actualizar y eliminar producto

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -9,7 +9,33 @@ const ProductController = {
             console.error(error);
             res.status(500).send(error);
         }
-    }
+    },
+    async update(req, res) {
+        try {
+            await Product.update(req.body, {
+                where: {
+                    id: req.params.id,
+                }
+            });
+            res.status(201).send({msg: "Product updated successfully"});
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async delete(req, res) {
+        try {
+            await Product.destroy({
+                where: {
+                    id: req.params.id,
+                },
+            });
+            res.send("Product deleted successfully");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
 };
 
 module.exports = ProductController;
--- routes/products.js
+++ routes/products.js
@@ -3,5 +3,7 @@ const ProductController = require("../controllers/ProductController");
 const router = express.Router();
 
 router.post("/", ProductController.create);
+router.put("/id/:id", ProductController.update);
+router.delete("/id/:id", ProductController.delete);
 
 module.exports = router;
