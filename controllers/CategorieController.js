c9c266670917b207243c2692d3333b40e788929e
manudana11 <manueldanapallardo@gmail.com>
2024-05-06 00:33:55 +0200

    categoria eliminar, actualizar y traer

--- controllers/CategorieController.js
+++ controllers/CategorieController.js
@@ -1,10 +1,47 @@
-const { Categorie } = require("../models/index");
+const { Categorie, Product } = require("../models/index");
 
 const CategorieController = {
     async create(req, res) {
         try {
             const categorie = await Categorie.create(req.body);
-            res.status(201).send({msg: "Categorie created succesfully", categorie});
+            res.status(201).send({msg: "Categorie created successfully", categorie});
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async update(req, res) {
+        try {
+            await Categorie.update(req.body, {
+                where: {
+                    id: req.params.id,
+                }
+            });
+            res.status(202).send({msg: "Categorie updated successfully"})
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async delete(req, res) {
+        try {
+            await Categorie.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+            res.send("Categorie deleted.");
+        } catch (error) {
+            console.error(error);
+            res.status(500).send(error);
+        }
+    },
+    async getAll(req, res) {
+        try {
+            const categorie = await Categorie.findAll({
+                include:[{model: Product,attributes:["name"], through: {attributes: []}}]
+            });
+            res.send(categorie);
         } catch (error) {
             console.error(error);
             res.status(500).send(error);
--- routes/categories.js
+++ routes/categories.js
@@ -3,5 +3,8 @@ const CategorieController = require("../controllers/CategorieController");
 const router = express.Router();
 
 router.post("/", CategorieController.create);
+router.put("/id/:id", CategorieController.update);
+router.delete("/id/:id", CategorieController.delete);
+router.get("/", CategorieController.getAll);
 
 module.exports = router;
