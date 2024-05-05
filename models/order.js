087eecebb10485ab20c57ee576fd99b08587728f
manudana11 <manueldanapallardo@gmail.com>
2024-05-05 19:41:07 +0200

    añadimos todas las relaciones

--- models/order.js
+++ models/order.js
@@ -4,13 +4,12 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Order extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
       Order.belongsTo(models.User);
+      Order.belongsToMany(models.Product, {
+        through:models.OrderUser
+      });
     }
   }
   Order.init({
--- models/product.js
+++ models/product.js
@@ -4,13 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+    
     static associate(models) {
-      // define association here
+      Product.belongsToMany(models.Order, {
+        through:models.OrderUser
+      });
+      Product.belongsToMany(models.Categorie, {
+        through:models.ProductCategorie
+      })
     }
   }
   Product.init({
