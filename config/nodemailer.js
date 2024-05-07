11f39b73c3d3720dc4838f72b9d3ad89ebcc3248
manudana11 <manueldanapallardo@gmail.com>
2024-05-07 20:54:55 +0200

    medio solucionado

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const { user, pass} = require("./config.json")["development"];
 let transporter = nodemailer.createTransport({
     host: 'smtp.office365.com',
     port: 587,
